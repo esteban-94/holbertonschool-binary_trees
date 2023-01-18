e59ce5f8b02155191435e3c14bcdb4e50fedde90
esteban <juan.esteban.94@outlook.com>
2023-01-18 09:54:36 -0500

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,10 +10,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (true);
+		return (1);
 
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
-	return (false);
+	return (0);
 }
