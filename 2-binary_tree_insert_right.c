ea5e9f506c4c2379c1552bdee9a3ef3f88ddb149
esteban <juan.esteban.94@outlook.com>
2023-01-19 09:16:09 -0500

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,32 +2,24 @@
 /**
  * binary_tree_insert_right - function that inserts a node as the right-cild
  * of another code
- *
  * @parent: is a pointer to the node to insert to the left-child
  * @value: is the value to store in the new node
- *
  * Return: a pointer to the created node, or NULL on failure or if parent is
  * NULL
- *
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-
 	binary_tree_t *newNode;
 
 	if (parent == NULL)
 		return (NULL);
-
 	newNode = malloc(sizeof(binary_tree_t));
-
 	if (newNode == NULL)
 		return (NULL);
-
 	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
 	newNode->parent = parent;
-
 	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
